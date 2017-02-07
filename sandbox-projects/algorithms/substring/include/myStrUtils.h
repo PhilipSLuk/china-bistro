3f27d2564e865f4e7495008551e97bfe1a197af2
Philip Luk <fortybobby@gmail.com>
2017-02-07 17:07:41 -0500

    skeleton c project

---
+++ sandbox-projects/algorithms/substring/include/myStrUtils.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+char* findLongestCommonSubString(char* str1, char* str2);
---
+++ sandbox-projects/algorithms/substring/myStrUtils.c
@@ -0,0 +1,6 @@
+#include <string.h>
+#include "myStrUtils.h"
+
+char* findLongestCommonSubString(char* str1, char* str2) {
+  return str2;
+}
